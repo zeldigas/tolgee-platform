645119d657d57106d7d051130eda346a7bc36dd0
Dmitriy Pavlov <zeldigas@gmail.com>
2026-05-30 18:35:17 -0400

    Removed new ee features

--- ee/backend/app/src/main/kotlin/io/tolgee/ee/service/qa/QaIssueBulkInsert.kt
+++
@@ -1,9 +0,0 @@
-package io.tolgee.ee.service.qa
-
-import io.tolgee.model.enums.qa.QaIssueState
-
-data class QaIssueBulkInsert(
-  val translationId: Long,
-  val result: QaCheckResult,
-  val state: QaIssueState,
-)
