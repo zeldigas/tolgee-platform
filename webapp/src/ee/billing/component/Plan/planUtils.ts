3508391d66e65c1fec3a945639ac53d82c35a2e7
Dmitriy Pavlov <zeldigas@gmail.com>
2026-06-03 14:53:41 -0400

    Cleaning up ee folders after upstream merge

--- webapp/src/ee/billing/component/Plan/planUtils.ts
+++
@@ -1,4 +0,0 @@
-import { Theme } from '@mui/material';
-
-export const getHighlightColor = (theme: Theme, custom: boolean) =>
-  custom ? theme.palette.tokens.info.main : theme.palette.tokens.primary.main;
